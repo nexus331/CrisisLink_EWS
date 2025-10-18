7a72dc93a1ccb83f215c001a1ba2392320425574
nexus331 <memelordyt53@gmail.com>
2025-10-18 21:42:22 +0800

    Updated CrisisLink Files

--- CrisisLink/clclient.lua
+++ CrisisLink/clclient.lua
@@ -2,22 +2,20 @@ local modem = peripheral.find("modem", rednet.open)
 local printer = peripheral.find("printer")
 print("CrisisLink EWS Reciever")
 print("Scanning for Emergency Code")
+redstone.setOutput("left", true)
+
 while true do
 
-local id,message
-repeat
 
-redstone.setOutput("left", true)
-
-id, message = rednet.receive()
-until id == 0
-
-textutils.slowPrint(message)
+local id, message = rednet.receive("clpv1")
+print("Emergency Code Recieved:", message)
 redstone.setOutput("left", false)
 textutils.slowPrint("Emergency Warning System Activated")
-sleep(100)
+sleep(10)
 redstone.setOutput("left", true)
 textutils.slowPrint("Emergency Warning System Deactivated")
-
+sleep(4)
+shell.run("clear")
+print("CrisisLink EWS Reciever")
+print("Scanning for Emergency Code")
 end
-
---
+++ CrisisLink/clserver.lua
@@ -0,0 +1,21 @@
+local protocol, message
+
+local modem = peripheral.find("modem", rednet.open)
+
+print ("CrisisLink Emergency Warning System")
+print("Please Type the Following Emergency Code")
+textutils.slowPrint("ACT-EWS")
+
+while true do
+message = io.read()
+
+sleep(0.5)
+rednet.broadcast(message, "clpv1")
+term.write("Emergency Code Sent")
+textutils.slowPrint("Crisislink Emergency Warning System Activated")
+sleep(4)
+shell.run("clear")
+print ("CrisisLink Emergency Warning System")
+print("Please Type the Following Emergency Code")
+textutils.slowPrint("ACT-EWS")
+end
